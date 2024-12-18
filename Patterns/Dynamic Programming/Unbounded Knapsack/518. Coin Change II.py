7916b0c5a06f1724b8d47c9ebb865f1c3d49188f
ajithdevadas <ajithdevadastech@gmail.com>
2024-12-18 13:30:59 -0500

    12-18-2024

--- Patterns/Dynamic Programming/Unbounded Knapsack/518. Coin Change II.py
+++ Patterns/Dynamic Programming/Unbounded Knapsack/518. Coin Change II.py
@@ -6,27 +6,18 @@ class Solution(object):
         :rtype: int
         """
 
-        if len(coins) == 1:
-            if coins[0] == amount:
-                return 1
-            else:
-                return 0
-
         self.dp = [0] * (amount+1)
-        self.dp[0] = 0
+        self.dp[0] = 1
 
-        for i in range(1,amount+1):
-            for j in range(len(coins)):
-                if i == coins[j]:
-                    self.dp[i] = self.dp[i] + 1
-                elif i - coins[j] >= 0:
-                    self.dp[i] = self.dp[i - coins[j]] + self.dp[i]
+        for coin in coins:
+            for j in range(coin, amount+1):
+                self.dp[j] = self.dp[j] + self.dp[j-coin]
 
         return self.dp[amount]
 
 
 o = Solution()
-amount = 5
-coins = [1,2,5]
+amount = 3
+coins = [2]
 print(o.change(amount, coins))
 
